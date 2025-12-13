19874441f94ddacf04fae159d22185b6637e1948
Soluchi Fidel-Ibeabuchi <fidelibeabuchi.soluchi@gmail.com>
2025-12-12 18:17:08 -0600

    postgres schema edited

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,14 +1,39 @@
-CREATE TABLE IF NOT EXISTS studios (
-    studio_id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
+CREATE TABLE studios (
+  studio_id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS anime (
-    anime_id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    synopsis TEXT,
-    year INT,
-    popularity INT,
-    score FLOAT,
-    studio_id INT REFERENCES studios(studio_id)
+CREATE TABLE anime (
+  anime_id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  synopsis TEXT,
+  year INT,
+  popularity INT,
+  score FLOAT,
+  studio_id INT REFERENCES studios(studio_id)
+);
+
+CREATE TABLE genres (
+  genre_id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE
+);
+
+CREATE TABLE anime_genres (
+  anime_id INT REFERENCES anime(anime_id),
+  genre_id INT REFERENCES genres(genre_id),
+  PRIMARY KEY (anime_id, genre_id)
+);
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE,
+  country TEXT
+);
+
+CREATE TABLE user_ratings (
+  user_id INT REFERENCES users(user_id),
+  anime_id INT REFERENCES anime(anime_id),
+  rating FLOAT,
+  timestamp TIMESTAMP,
+  PRIMARY KEY (user_id, anime_id)
 );
